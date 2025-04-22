17ee56ac296e84af621eb8cb37312135dfa2ea85
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-21 22:56:35 -0400

    todo list

---
+++ main.py
@@ -0,0 +1,13 @@
+# todo - 1. create screen
+# todo - 2. create and move paddle
+# todo - 3. create another paddle
+# todo - 4. crete ball and make it move
+# todo - 5. detect collision with wall and bounce
+# todo - 6. detect collision with paddle
+# todo - 7. detect when paddle misses
+# todo - 8. keep score
+
+
+
+
+
