5e25e1fb0e2e3f0f5a054e891d73393f83a82cf6
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-22 19:33:43 -0400

    screen setup

--- main.py
+++ main.py
@@ -6,8 +6,12 @@
 # todo - 6. detect collision with paddle
 # todo - 7. detect when paddle misses
 # todo - 8. keep score
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title('Pong Game')
 
 
-
-
-
+screen.exitonclick()
