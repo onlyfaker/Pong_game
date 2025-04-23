2a2d2a01e4bc76274ea25fbd11fd0bdd1dba008a
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-22 20:13:48 -0400

    small paddle update

--- main.py
+++ main.py
@@ -6,12 +6,34 @@
 # todo - 6. detect collision with paddle
 # todo - 7. detect when paddle misses
 # todo - 8. keep score
-from turtle import Screen
+from turtle import Screen, Turtle
+# from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title('Pong Game')
+
+paddle_left = Turtle()
+paddle_left.penup()
+paddle_left.goto(350,y=0)
+paddle_left.color("white")
+paddle_left.shape("square")
+paddle_left.shapesize(stretch_len=1,stretch_wid=5)
+
+screen.listen()
+
+def move_up():
+    new_y = paddle_left.ycor()+20
+    paddle_left.goto(paddle_left.xcor(),new_y)
+def move_down():
+    new_y = paddle_left.ycor() + -20
+    paddle_left.goto(paddle_left.xcor(), new_y)
+
+screen.onkeypress(move_up,"Up")
+screen.onkeypress(move_down,"Down")
+
+
 
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,10 @@
+# from turtle import Turtle
+# class Paddle(Turtle()):
+#     def __init__(self):
+#         super(Paddle, self).__init__()
+#
+#
+#
+#
+#
+#
