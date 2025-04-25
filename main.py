802ab7b2e03001a6113fa6c4859c654c69624353
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-24 21:10:17 -0400

    paddle update

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
-# todo - 1. create screen
-# todo - 2. create and move paddle
-# todo - 3. create another paddle
+# todo/done - 1. create screen
+# todo/done - 2. create and move paddle
+# todo/done - 3. create another paddle
 # todo - 4. crete ball and make it move
 # todo - 5. detect collision with wall and bounce
 # todo - 6. detect collision with paddle
 # todo - 7. detect when paddle misses
 # todo - 8. keep score
 from turtle import Screen, Turtle
-# from paddle import Paddle
+
+from paddle import Paddle
+
 
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title('Pong Game')
 screen.tracer(0)
-
-
-paddle_left = Turtle()
-
-paddle_left.penup()
-paddle_left.goto(350,y=0)
-paddle_left.color("white")
-paddle_left.shape("square")
-paddle_left.shapesize(stretch_len=1,stretch_wid=5)
 screen.update()
 screen.listen()
 
-def move_up():
-    new_y = paddle_left.ycor()+20
-    paddle_left.goto(paddle_left.xcor(),new_y)
-def move_down():
-    new_y = paddle_left.ycor() + -20
-    paddle_left.goto(paddle_left.xcor(), new_y)
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
 
-screen.onkeypress(move_up,"Up")
-screen.onkeypress(move_down,"Down")
-
+# TODO - at the end FIX the issue where there is a LAG before i can move with onkeypress!!!!
+screen.onkeypress(right_paddle.move_up, "Up")
+screen.onkeypress(right_paddle.move_down, "Down")
+screen.onkeypress(left_paddle.move_up, "w")
+screen.onkeypress(left_paddle.move_down, "s")
 
 
 game_on = True
 while game_on:
+
     screen.update()
 
 
--- paddle.py
+++ paddle.py
@@ -1,10 +1,19 @@
-# from turtle import Turtle
-# class Paddle(Turtle()):
-#     def __init__(self):
-#         super(Paddle, self).__init__()
-#
-#
-#
-#
-#
-#
+from turtle import Turtle
+class Paddle(Turtle):
+    def __init__(self,position):
+        super(Paddle, self).__init__()
+
+        self.penup()
+        self.goto(350,0)
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.goto(position)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    def move_down(self):
+        new_y = self.ycor() + -20
+        self.goto(self.xcor(), new_y)
+
